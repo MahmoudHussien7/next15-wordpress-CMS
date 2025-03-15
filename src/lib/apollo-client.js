3b70000dcb76aa8da2631e2873ff6fc211115e0d
Mahmoud Hussien Abdulhaliem <mahmoudhu37@gmail.com>
2025-03-15 06:04:17 +0200

    docs: :memo: apollo client uri for your wordpress

--- src/lib/apollo-client.js
+++ src/lib/apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://hrawy.local/graphql",
+  uri: "PLACE IT WITH YOUR REAL URL",
   cache: new InMemoryCache(),
 });
 
